fb03e5e31e84813e10c610d357ad2b8ff24574b4
Santiago Martinez <santiago.mbal@gmail.com>
2023-02-12 19:12:28 +0000

    create a module for artist extraction

---
+++ src/metamoth/artist.py
@@ -0,0 +1,95 @@
+"""Functions for reading the artist chunk from a WAV file."""
+from typing import BinaryIO, Optional
+
+from metamoth.chunks import Chunk
+
+__all__ = [
+    "get_am_artist",
+]
+
+
+def read_artist(wav: BinaryIO, artist_chunk: Chunk) -> str:
+    """Return the artist from the artist chunk.
+
+    Parameters
+    ----------
+    wav : BinaryIO
+        Open file object of the WAV file.
+
+    artist_chunk : Chunk
+        The artist chunk
+
+    Returns
+    -------
+    artist: str
+    """
+    wav.seek(artist_chunk.position + 8)
+    comment = wav.read(artist_chunk.size - 4).decode("utf-8").strip("\x00")
+    return comment
+
+
+def get_artist_chunk(chunk: Chunk) -> Optional[Chunk]:
+    """Get the artist chunk from the RIFF chunk.
+
+    Parameters
+    ----------
+    chunk : Chunk
+        The RIFF chunk, which is the root chunk.
+
+    Returns
+    -------
+    Chunk
+        The artist chunk. If the chunk is not found, returns None.
+    """
+    for subchunk in chunk.subchunks:
+        if subchunk.chunk_id == "LIST":
+            break
+    else:
+        return None
+
+    for subsubchunk in subchunk.subchunks:
+        if subsubchunk.chunk_id == "IART":
+            return subsubchunk
+
+    return None
+
+
+def get_audiomoth_id_from_artist(comment: str) -> str:
+    """Get the AudioMoth ID from the artist string.
+
+    Parameters
+    ----------
+    comment : str
+        The artist string.
+
+    Returns
+    -------
+    str
+        The AudioMoth ID.
+    """
+    return comment.split(" ")[-1]
+
+
+def get_am_artist(wav: BinaryIO, chunk: Chunk) -> Optional[str]:
+    """Get the artist string from the WAV file.
+
+    Parameters
+    ----------
+    wav : BinaryIO
+        The WAV file.
+
+    chunk : Chunk
+        The RIFF chunk, which is the root chunk.
+
+    Returns
+    -------
+    str
+        The artist string. If the artist is not found, returns None.
+    """
+    artist_chunk = get_artist_chunk(chunk)
+
+    if artist_chunk is None:
+        return None
+
+    artist = read_artist(wav, artist_chunk)
+    return get_audiomoth_id_from_artist(artist)
---
+++ tests/test_artist.py
@@ -0,0 +1,24 @@
+"""Test the artist module."""
+import os
+
+from metamoth.artist import get_am_artist
+from metamoth.chunks import parse_into_chunks
+
+PACKAGE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+DATA_DIR = os.path.join(PACKAGE_DIR, "data")
+
+
+def test_get_am_artist_when_missing():
+    """Test the get_am_artist function."""
+    with open(os.path.join(DATA_DIR, "test.wav"), "rb") as wav:
+        riff = parse_into_chunks(wav)
+        artist = get_am_artist(wav, riff)
+        assert artist is None
+
+
+def test_get_am_artist():
+    """Test the get_am_artist function."""
+    with open(os.path.join(DATA_DIR, "test_artist.wav"), "rb") as wav:
+        riff = parse_into_chunks(wav)
+        artist = get_am_artist(wav, riff)
+        assert artist == '243B1F055B2BF663'
